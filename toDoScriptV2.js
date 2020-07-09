4d9f0e368fdd6a27e489e2967a82edaf0ebb0219
Shahryar Shagoshtasbi <shahryar9782@gmail.com>
2020-07-09 12:45:09 -0400

    staging

--- toDoScriptV2.js
+++ toDoScriptV2.js
@@ -1,1 +1,33 @@
+toDoList = [];
+trash = [];
+let num = 0;
 
+function addTask(task) {
+  let data = {
+    text: task,
+    completed: false,
+    idNum: num++,
+  };
+  toDoList.push(data);
+}
+function makeFoo() {
+  for (i = 0; i < 11; i++) {
+    addTask("Task " + i);
+  }
+}
+makeFoo();
+
+function deleteTask(removeId) {
+  let foundIndex = toDoList.findIndex((el) => {
+    return el.idNum === removeId;
+  });
+  toDoList.splice(foundIndex, 1);
+}
+// deleteTask(2);
+
+console.log(JSON.stringify(toDoList));
+
+// add
+// delete
+// edit
+// clear all
